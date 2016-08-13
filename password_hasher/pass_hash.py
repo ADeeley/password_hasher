99bb5b1e98b04b8336cee555434b79aaac44aed7
Adam Deeley <ad.deeley@gmail.com>
2016-08-13 13:39:16 +0100

    Added a function to change the destination file
    Signed-off-by: Adam Deeley <ad.deeley@gmail.com>

--- password_hasher/pass_hash.py
+++ password_hasher/pass_hash.py
@@ -4,24 +4,28 @@ import bcrypt
 # To do:
 # Add a checking system to hash_pwd to check if the password has already been 
 # hashed- use check_pwd function
+# add an option to change the file destination.
+# take out hard coding from the script
 
 #------------------
 
-def hash_pwd():
+destinationFile = "keys.txt"
+
+def hash_pwd(destinationFile):
     '''Hash a password for the first time, with a randomly-generated salt'''
     
-    keysFile = open("keys.txt", "a")
-    newPwd = raw_input("\nPlease type the password you would like to hash.>")
+    keysFile = open(destinationFile, "a")
+    newPwd = raw_input("\nPlease type the password you would like to hash.\n>")
     hashed = bcrypt.hashpw(newPwd, bcrypt.gensalt(10))  
-    print "\nYour hashed password is: ", hashed, "\nSaved to file \"keys.txt\""
+    print "\nYour hashed password is: ", hashed, "\nSaved to file ", destinationFile
     keysFile.write(hashed+ "\n")
 
-def check_pwd():        
+def check_pwd(destinationFile):        
     ''' checks if the plain-text password is in the database. returns 
     True or False'''
     
     match = False
-    keysFile = open("keys.txt", "r")
+    keysFile = open(destinationFile, "r")
     Pwd = raw_input("\nPlease type the password you would like to check: \n>")
     for hash in keysFile:      
 
@@ -34,12 +38,17 @@ def check_pwd():
 
         keysFile.close()
 
+def change_destination(destinationFile):
+    destinationFile = raw_input("Type the path of the destination file you would like to use: ")       
+    print "\nThe destination file has been updated to: ", destinationFile
+    
 def engine():        
     while True:
         choice = int(raw_input("""\nWould you like to: 
 (1) Hash a new password 
-(2) Check a currently hashed one?
-(3) Quit program
+(2) Check a currently hashed one
+(3) Change destination file
+(4) Quit program
 >"""))
             
         if choice == 1: 
@@ -47,6 +56,8 @@ def engine():
         elif choice == 2:
             check_pwd()
         elif choice == 3:
+            change_destination()
+        elif choice == 4:
             print "\nThanks for hashing!"
             break        
 
