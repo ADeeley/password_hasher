acb86831cbddd8a85d572020c1b4a846d25b1462
Adam Deeley <ad.deeley@gmail.com>
2016-08-13 13:15:21 +0100

    Added function wrappers and general tidying.
    Signed-off-by: Adam Deeley <ad.deeley@gmail.com>

--- password_hasher/pass_hash.py
+++ password_hasher/pass_hash.py
@@ -1,37 +1,46 @@
 import bcrypt
 
-choice = int(raw_input("""Would you like to 
-(1) hash a new password 
-(2) Check a currently hashed one?"""))
-
-if choice == 1: #Hash a password for the first time, with a randomly-generated salt
-    keysFile = open("keys.txt", "a")
-    newPwd = raw_input("Please type the password you would like to hash: ")
-    hashed = bcrypt.hashpw(newPwd, bcrypt.gensalt(10))  
-    print "Your hashed password is: ", hashed, "\nSaved to file \"keys.txt\""
-    keysFile.write(hashed+ "\n")
+def hash_pwd():
+    '''Hash a password for the first time, with a randomly-generated salt'''
     
-# if choice == 2:
-    # keysFile = open("keys.txt", "r")
-    # hashed = keysFile.readline()
-    # Pwd = raw_input("Please type the password you would like to check: ")
-    # if bcrypt.hashpw(Pwd, hashed) == hashed.strip():
-        # print "It matches"
-    # else:
-        # print "It does not match"
-        
-if choice == 2: # checks if the plain-text password is in the database. returns True or False
+    keysFile = open("keys.txt", "a")
+    newPwd = raw_input("\nPlease type the password you would like to hash.>")
+    hashed = bcrypt.hashpw(newPwd, bcrypt.gensalt(10))  
+    print "\nYour hashed password is: ", hashed, "\nSaved to file \"keys.txt\""
+    keysFile.write(hashed+ "\n")
+
+def check_pwd():        
+    ''' checks if the plain-text password is in the database. returns 
+    True or False'''
+    
     match = False
     keysFile = open("keys.txt", "r")
-    Pwd = raw_input("Please type the password you would like to check: ")
+    Pwd = raw_input("\nPlease type the password you would like to check: \n>")
     for hash in keysFile:      
 
         if bcrypt.hashpw(Pwd, hash.strip()) == hash.strip():
             print True
             match = True
             break
-   
     if not match:
         print False
 
         keysFile.close()
+
+def engine():        
+    while True:
+        choice = int(raw_input("""\nWould you like to: 
+(1) Hash a new password 
+(2) Check a currently hashed one?
+(3) Quit program
+>"""))
+            
+        if choice == 1: 
+            hash_pwd()
+        elif choice == 2:
+            check_pwd()
+        elif choice == 3:
+            print "\nThanks for hashing!"
+            break        
+
+engine()            
