63097c96193411c869d46c939c323453ab0e3a27
Adam Deeley <ad.deeley@gmail.com>
2016-08-13 12:31:19 +0100

    Fixed the password checker (choice 2). It now returns True or False if the password is in the keys.txt file.
    Signed-off-by: Adam Deeley <ad.deeley@gmail.com>

--- password_hasher/pass_hash.py
+++ password_hasher/pass_hash.py
@@ -5,18 +5,33 @@ choice = int(raw_input("""Would you like to
 (2) Check a currently hashed one?"""))
 
 if choice == 1: #Hash a password for the first time, with a randomly-generated salt
-    keysFile = open("keys.txt", "w")
+    keysFile = open("keys.txt", "a")
     newPwd = raw_input("Please type the password you would like to hash: ")
     hashed = bcrypt.hashpw(newPwd, bcrypt.gensalt(10))  
     print "Your hashed password is: ", hashed, "\nSaved to file \"keys.txt\""
-    keysFile.writelines(hashed)
+    keysFile.write(hashed+ "\n")
     
-if choice == 2:
+# if choice == 2:
+    # keysFile = open("keys.txt", "r")
+    # hashed = keysFile.readline()
+    # Pwd = raw_input("Please type the password you would like to check: ")
+    # if bcrypt.hashpw(Pwd, hashed) == hashed.strip():
+        # print "It matches"
+    # else:
+        # print "It does not match"
+        
+if choice == 2: # checks if the plain-text password is in the database. returns True or False
+    match = False
     keysFile = open("keys.txt", "r")
-    hashed = keysFile.readline()
     Pwd = raw_input("Please type the password you would like to check: ")
-    if bcrypt.hashpw(Pwd, hashed) == hashed:
-        print "It matches"
-    else:
-        print "It does not match"
-keysFile.close()
+    for hash in keysFile:      
+
+        if bcrypt.hashpw(Pwd, hash.strip()) == hash.strip():
+            print True
+            match = True
+            break
+   
+    if not match:
+        print False
+
+        keysFile.close()
